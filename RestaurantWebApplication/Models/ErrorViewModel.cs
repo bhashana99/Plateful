12fe2bc716656a0080e766e5ae6f5082b70b56b3
bhashana99 <bhashanachamodya99@gmail.com>
2025-06-07 16:36:55 +0530

    first commit

---
+++ RestaurantWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
