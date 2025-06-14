db5b6bf207321adfdd0b09b7b318892764894ffe
bhashana99 <bhashanachamodya99@gmail.com>
2025-06-14 13:34:32 +0530

    Seed initial data for Categories and Ingredients

--- RestaurantWebApplication/Data/ApplicationDbContext.cs
+++ RestaurantWebApplication/Data/ApplicationDbContext.cs
@@ -20,20 +20,40 @@ namespace RestaurantWebApplication.Data
 
         public DbSet<ProductIngredient> ProductIngredients { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(modelBuilder);
             // Configure the many-to-many relationship between Product and Ingredient
-            builder.Entity<ProductIngredient>()
+            modelBuilder.Entity<ProductIngredient>()
                 .HasKey(pi => new { pi.ProductId, pi.IngredientId });
-            builder.Entity<ProductIngredient>()
+            modelBuilder.Entity<ProductIngredient>()
                 .HasOne(pi => pi.Product)
                 .WithMany(p => p.ProductIngredients)
                 .HasForeignKey(pi => pi.ProductId);
-            builder.Entity<ProductIngredient>()
+            modelBuilder.Entity<ProductIngredient>()
                 .HasOne(pi => pi.Ingredient)
                 .WithMany(i => i.ProductIngredients)
                 .HasForeignKey(pi => pi.IngredientId);
+
+
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category { CategoryId = 1, Name = "Appetizers" },
+                    new Category { CategoryId = 2, Name = "Entree" },
+                    new Category { CategoryId = 3, Name = "Side Dish" },
+                    new Category { CategoryId = 4, Name = "Dessert" },
+                    new Category { CategoryId = 5, Name = "Beverage" }
+                );
+
+            modelBuilder.Entity<Ingredient>()
+                .HasData(
+                    new Ingredient { IngredientId = 1, Name = "Beef" },
+                    new Ingredient { IngredientId = 2, Name = "Chicken" },
+                    new Ingredient { IngredientId = 3, Name = "Fish" },
+                    new Ingredient { IngredientId = 4, Name = "Tortilla" },
+                    new Ingredient { IngredientId = 5, Name = "Lettuce" },
+                    new Ingredient { IngredientId = 6, Name = "Tomato" }
+                );
         }
     }
 
